4322b8b24325de25f0c53c4342f5a0e61f5ea0e6
Michaelninder <fabianeternis@gmail.com>
2025-10-18 15:34:52 +0200

    feat: Make footer-component sticky via app-layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>@hasSection('title')@yield('title') | {{ config('app.name', 'Laravel') }}@else{{ config('app.name', 'Laravel') }}@endif</title>
+        <title>
+            @hasSection('title')@yield('title') |
+            {{ config('app.name', 'Laravel') }}@else{{ config('app.name', 'Laravel') }}@endif
+        </title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link
+            href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap"
+            rel="stylesheet"
+        />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @stack('head')
+        @vite(['resources/css/app.css', 'resources/js/app.js']) @stack('head')
     </head>
-    <body class="font-sans antialiased bg-gray-100 dark:bg-gray-900">
-        <div class="flex-grow bg-gray-100 dark:bg-gray-900">
-            @if (!isset($hideNavbar) || $hideNavbar !== true)
-                @include('components.navbar')
-            @endif
+    <body
+        class="font-sans antialiased bg-gray-100 dark:bg-gray-900 flex flex-col min-h-screen"
+    >
+        @if (!isset($hideNavbar) || $hideNavbar !== true)
+            @include('components.navbar')
+        @endif
 
+        <div class="flex-grow bg-gray-100 dark:bg-gray-900">
             @hasSection('header')
                 <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -41,7 +46,7 @@
         @if (!isset($hideScrollToTopBtn) || $hideScrollToTopBtn !== true)
             @include('components.scroll-to-top-btn')
         @endif
-        
+
         @stack('scripts')
     </body>
 </html>
