902630eedcf473c710efa4bcb8de2b27cba2e88a
Michaelninder <fabianeternis@gmail.com>
2025-10-21 09:51:19 +0200

    feat: Add store() to PanelOrganisationsContreoller

--- app/Http/Controllers/Panel/OrganisationController.php
+++ app/Http/Controllers/Panel/OrganisationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Organisation;
+use Illuminate\Support\Str;
 
 class OrganisationController extends Controller
 {
@@ -19,5 +20,29 @@ class OrganisationController extends Controller
     public function settings(Organisation $organisation)
     {
         return view('panel.organisations.settings', compact('organisation'));
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate(['name' => 'required|min:4|max:20',]);
+
+        $baseSlug = Str::slug($validatedData['name']);
+        $slug = $baseSlug;
+
+        while (Organisation::where('slug', $slug)->exists()) {
+            $slug = $baseSlug . '-' . Str::random(5);
+        }
+
+        $organisation = Organisation::create([
+            'name' => $validatedData['name'],
+            'email' => Auth::user()->email,
+            'user_id' => Auth::id(),
+            'slug' => $slug,
+        ]);
+
+        //$organisation->users()->attach(Auth::id(), ['role' => 'ownder']);
+        $organisation->users()->attach(Auth::id(), ['role' => 'admin']);
+
+        return redirect()->route('panel.organisations.overview', $organisation);
     }
 }
